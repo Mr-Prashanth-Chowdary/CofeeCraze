e4084f42e53063459d2fefa19057660575276694
Mr. Prashanth Chowdary <ch.prashanthchowdary007@gmail.com>
2025-02-17 13:30:17 +0530

    working cart with saving data in db.json

--- src/contextAPI/BuyItemCP.jsx
+++ src/contextAPI/BuyItemCP.jsx
@@ -1,9 +1,11 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import axios from 'axios'
 import BuyItemContext from './BuyItem'
 
 
 function BuyItemCP({children}) {
     const [buyItem,setBuyItem] = useState(null);
+
   return (
     <BuyItemContext.Provider value={{buyItem,setBuyItem}}>
         {children}
